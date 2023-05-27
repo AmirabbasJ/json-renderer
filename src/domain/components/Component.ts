85e04f478c4e6e0e23ad2f651a942da4a3c82171
AmirabbasJ <amirabbasgamer2@gmail.com>
2023-05-27 09:21:47 +0330

    fix component typings

--- src/domain/components/Component.ts
+++ src/domain/components/Component.ts
@@ -1,7 +1,6 @@
-export interface Component<
+export type Component<
   T extends string,
   O extends Record<string, any> | undefined = undefined,
-> {
+> = (O extends undefined ? { options?: never } : { options: O }) & {
   type: T;
-  options?: O;
-}
+};
